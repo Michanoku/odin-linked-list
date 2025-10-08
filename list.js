a3a5946bc21d3702ec8743478f009c415298a2c2
Michanoku <michael.werker@gmail.com>
2025-10-08 00:16:47 +0000

    Add toString function to test the list

--- list.js
+++ list.js
@@ -1,21 +1,48 @@
 class LinkedList {
   constructor(head=null) {
+    // Set the head to null at first
     this.head = head;
   }
+
   append(value) {
+    // Create the new node with the value
     const newNode = new Node(value, null);
-    const tail = this.tail(this.head);
-    tail.nextNode = newNode;
+    // Check if the head exists
+    if (this.head) {
+      // Use the tail function to find the tail of the list
+      const tail = this.tail(this.head);
+      // Point the tail of the list at the new node, making it the new tail
+      tail.nextNode = newNode;
+    } else {
+      // The new node will be the head
+      this.head = newNode;
+    }
+
   }
 
   tail(node) {
+    // If the current node does not point at anything, return it
     if (node.nextNode === null) {
       return node;
     }
+    // Otherwise check the next node
     return this.tail(node.nextNode);
   }
-}
 
+  toString() {
+    // Start at the head with an empty string
+    let current = this.head;
+    let listString = '';
+    // As long as the current node points at another node, concat the value
+    while(current.nextNode !== null) {
+      listString = listString.concat(`( ${current.value} ) -> `);
+      current = current.nextNode;
+    } 
+    // Once the end is reached, concat null and log the string
+    listString = listString.concat('null');
+    console.log(listString);
+  }
+}
 
 class Node {
   constructor(value=null, nextNode=null) {
@@ -24,3 +51,4 @@ class Node {
   };
 
 }
+export { LinkedList }
